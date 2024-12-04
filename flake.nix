f48313e7146b18a569139f7c6e476249729b4e7e
Aleksey Sidorov <sauron1987@gmail.com>
2024-12-04 13:14:38 +0300

    Bump msrv

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           rust-overlay.overlays.default
           (final: prev: {
             rustToolchains = {
-              msrv = prev.rust-bin.stable."1.75.0".default;
+              msrv = prev.rust-bin.stable."1.78.0".default;
               stable = prev.rust-bin.stable.latest.default.override {
                 extensions = [
                   "rust-src"
@@ -79,7 +79,7 @@
         semver_checks = writeShellApplication {
           name = "ci-run-semver-checks";
           runtimeInputs = with pkgs; [
-            rustToolchains.stable
+            rustToolchains.msrv
             cargo-semver-checks
           ] ++ runtimeInputs;
           text = ''cargo semver-checks'';
